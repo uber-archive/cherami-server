63bd3848fb17fc0d07b5ac7db387cfab5ca584e0
kiranrg <kiranrg@uber.com>
2017-03-24 14:13:35 -0700

    make log elk friendly

--- common/configure/commonconfigure.go
+++ common/configure/commonconfigure.go
@@ -233,7 +233,7 @@ func (r *CommonConfigure) SetupServerConfig() CommonAppConfig {
 			if net.ParseIP(listenAddress) == nil {
 				// default ListenAddress is not set, try obtain from TChannel
 				ip, _ := tchannel.ListenIP()
-				log.WithFields(`local-ip`, ip).Info(`No listen address specified; using local ip instead`)
+				log.WithField(`tchannel-ip`, ip).Info(`No listen address specified; using tchannel-ip instead`)
 				if ip == nil {
 					log.Panic(`Could not get listen address`)
 				}
