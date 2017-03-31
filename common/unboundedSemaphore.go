38ee9207688da5e70869ef3966d97a6295f821ab
Guillaume Bailey <gbailey@uber.com>
2017-03-30 17:06:24 -0700

    CR comments, testing for unboundedSemaphore

--- common/unboundedSemaphore.go
+++ common/unboundedSemaphore.go
@@ -24,17 +24,23 @@ import "sync"
 
 // UnboundedSemaphore operates similarly to traditional Go channel-based semaphores,
 // but without any limitation on the number of resources that can be released
+//
+// See also: https://godoc.org/github.com/dropbox/godropbox/sync2#NewUnboundedSemaphore
+//
 type UnboundedSemaphore struct {
-	resCount    int
-	once        sync.Once
-	c           *sync.Cond
-	m           sync.Mutex // Don't use this directly, use through s.c.L
-	acquireLock sync.Mutex // Used by acquirer for credit accumulation
+	resCount        int
+	once            sync.Once
+	c               *sync.Cond
+	m               sync.Mutex // need to name this so it's not exported. Don't use this directly, use through s.c.L
+	accumulatorLock sync.Mutex // Used by acquirer for credit accumulation
 }
 
 // Release releases n resources
 func (s *UnboundedSemaphore) Release(n int) {
 	s.once.Do(s.init)
+	if n <= 0 {
+		return
+	}
 	s.c.L.Lock()
 	s.resCount += n
 	s.c.L.Unlock()
@@ -59,13 +65,14 @@ func (s *UnboundedSemaphore) Acquire(n int) {
 	}
 
 	// Ensure that only one thread is accumulating resources at one time;
-	// Prevents acquire(1000)+acquire(1000)+release(500)+release(500) from deadlocking
+	// Prevents acquire(700)+acquire(300)+release(500)+release(500) from deadlocking
 	// This may not work very well with high contention; it may take a long time to accumulate resources if there are
-	// many threads acquiring without accumulation
-	s.acquireLock.Lock()
+	// many competing threads acquiring without accumulation
+	s.accumulatorLock.Lock()
+	s.c.L.Lock()
 	for n > 0 { // While we are waiting for more resources
-		s.c.L.Lock()
 		for s.resCount <= 0 { // Wait for resources to be available
+		//	fmt.Println(`Waiting to acummulate `, n)
 			s.c.Wait()
 		}
 		if n >= s.resCount { // If we have just enough or too few resources, grab them all and maybe keep accumulating
@@ -76,8 +83,8 @@ func (s *UnboundedSemaphore) Acquire(n int) {
 			n = 0
 		}
 	}
-	s.acquireLock.Unlock()
 	s.c.L.Unlock()
+	s.accumulatorLock.Unlock()
 }
 
 func (s *UnboundedSemaphore) init() {
