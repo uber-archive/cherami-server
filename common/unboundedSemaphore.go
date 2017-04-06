b1bf130d6dd7deacc9bf3b6147bde6a4682a95a0
Guillaume Bailey <github-guynoir@snkmail.com>
2017-04-05 18:57:55 -0700

    kafka stream implementation, import Kafka libs (#116)
    * kafka stream implementation, import Kafka libs
    
    * CR comments, testing for unboundedSemaphore
    
    * CR
    
    * Add KafkaStream test

---
+++ common/unboundedSemaphore.go
@@ -0,0 +1,92 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package common
+
+import "sync"
+
+// UnboundedSemaphore operates similarly to traditional Go channel-based semaphores,
+// but without any limitation on the number of resources that can be released
+//
+// See also: https://godoc.org/github.com/dropbox/godropbox/sync2#NewUnboundedSemaphore
+//
+type UnboundedSemaphore struct {
+	resCount        int
+	once            sync.Once
+	c               *sync.Cond
+	m               sync.Mutex // need to name this so it's not exported. Don't use this directly, use through s.c.L
+	accumulatorLock sync.Mutex // Used by acquirer for credit accumulation
+}
+
+// Release releases n resources
+func (s *UnboundedSemaphore) Release(n int) {
+	s.once.Do(s.init)
+	if n <= 0 {
+		return
+	}
+	s.c.L.Lock()
+	s.resCount += n
+	s.c.L.Unlock()
+	s.c.Broadcast() // Need broadcast, not signal, as we may satisfy many acquisitions
+}
+
+// Acquire acquires n resources. If n is negative, there is no effect.
+func (s *UnboundedSemaphore) Acquire(n int) {
+	s.once.Do(s.init)
+	if n <= 0 {
+		return
+	}
+
+	if n == 1 { // Non-accumulating case
+		s.c.L.Lock()
+		for s.resCount <= 0 {
+			s.c.Wait()
+		}
+		s.resCount--
+		s.c.L.Unlock()
+		return
+	}
+
+	// Ensure that only one thread is accumulating resources at one time;
+	// Prevents acquire(700)+acquire(300)+release(500)+release(500) from deadlocking
+	// This may not work very well with high contention; it may take a long time to accumulate resources if there are
+	// many competing threads acquiring without accumulation
+	s.accumulatorLock.Lock()
+	s.c.L.Lock()
+	for n > 0 { // While we are waiting for more resources
+		for s.resCount <= 0 { // Wait for resources to be available
+		//	fmt.Println(`Waiting to acummulate `, n)
+			s.c.Wait()
+		}
+		if n >= s.resCount { // If we have just enough or too few resources, grab them all and maybe keep accumulating
+			n -= s.resCount
+			s.resCount = 0
+		} else { // If we have more resources than required, just subtract what we need and exit
+			s.resCount -= n
+			n = 0
+		}
+	}
+	s.c.L.Unlock()
+	s.accumulatorLock.Unlock()
+}
+
+func (s *UnboundedSemaphore) init() {
+	s.c = sync.NewCond(&s.m)
+}
---
+++ common/unboundedSemaphore_test.go
@@ -0,0 +1,304 @@
+// Copyright (c) 2016 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package common
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type UnboundedSemaphoreSuite struct {
+	*require.Assertions // override suite.Suite.Assertions with require.Assertions; this means that s.NotNil(nil) will stop the test, not merely log an error
+	suite.Suite
+}
+
+func TestUnboundedSemaphoreSuite(t *testing.T) {
+	suite.Run(t, new(UnboundedSemaphoreSuite))
+}
+
+func (s *UnboundedSemaphoreSuite) SetupTest() {
+	s.Assertions = require.New(s.T()) // Have to define our overridden assertions in the test setup. If we did it earlier, s.T() will return nil
+}
+
+const longTime = time.Second
+const shortTime = time.Millisecond * 10
+
+func (s *UnboundedSemaphoreSuite) TestNoRelease() {
+	var u UnboundedSemaphore
+
+	u.Release(10)
+	u.Acquire(5)
+	u.Acquire(5)
+
+	closeCh := make(chan struct{}, 0)
+
+	go func() {
+		u.Acquire(1)
+		close(closeCh)
+	}()
+
+	select {
+	case <-time.After(shortTime):
+	case <-closeCh:
+		s.Fail(`Acquire should block with insufficient resources`)
+	}
+
+	u.Release(1)
+	<-closeCh
+}
+
+func (s *UnboundedSemaphoreSuite) TestNegativeRelease() {
+	var u UnboundedSemaphore
+
+	u.Release(-10)
+
+	closeCh := make(chan struct{}, 0)
+
+	go func() {
+		u.Acquire(1)
+		close(closeCh)
+	}()
+
+	select {
+	case <-time.After(shortTime):
+	case <-closeCh:
+		s.Fail(`Acquire should block with insufficient resources`)
+	}
+
+	u.Release(1)
+	select {
+	case <-time.After(shortTime):
+		s.Fail(`Acquire should have succeeded`)
+	case <-closeCh:
+	}
+}
+
+func (s *UnboundedSemaphoreSuite) TestNegativeAcquire() {
+	var u UnboundedSemaphore
+	var closeCh chan struct{}
+
+	acquireAndClose := func() {
+		u.Acquire(1)
+		close(closeCh)
+	}
+
+	u.Release(1)
+	u.Acquire(-10000)
+
+	closeCh = make(chan struct{}, 0)
+	go acquireAndClose()
+	select {
+	case <-time.After(shortTime):
+		s.Fail(`Acquire should have succeeded`)
+	case <-closeCh:
+	}
+
+	closeCh = make(chan struct{}, 0)
+	go acquireAndClose()
+	select {
+	case <-time.After(shortTime):
+	case <-closeCh:
+		s.Fail(`Acquire should have failed`)
+	}
+
+	u.Release(1)
+	<-closeCh
+}
+
+func (s *UnboundedSemaphoreSuite) TestAccumulatorWins() {
+	contentionScale := 100
+	var u UnboundedSemaphore
+
+	closeCh := make(chan struct{}, 0)
+
+	// Make a bunch of concurrent non-accumulating contenders
+	for i := 0; i < contentionScale; i++ {
+		go func() {
+			for {
+				select {
+				case <-closeCh:
+					return
+				default:
+					u.Acquire(1)
+					u.Release(1)
+				}
+			}
+		}()
+	}
+
+	// Also concurrently, try to acquire everything
+	go func() {
+		u.Acquire(contentionScale)
+		close(closeCh)
+		u.Release(contentionScale)
+	}()
+
+	// Slowly release the full resources
+	for i := 0; i < contentionScale; i++ {
+		u.Release(1)
+	}
+
+	select {
+	case <-time.After(longTime):
+		s.Fail(`Accumulative acquire should win in reasonable time`)
+		close(closeCh)
+		u.Release(contentionScale * 10)
+	case <-closeCh:
+	}
+}
+
+func (s *UnboundedSemaphoreSuite) TestManyAccumulatorsWin() {
+	contentionScale := 100
+	var u UnboundedSemaphore
+	var wg sync.WaitGroup
+
+	closeCh := make(chan struct{}, 0)
+
+	// one-shot contender
+	catchAndRelease := func(i int) {
+		u.Acquire(i)
+		u.Release(i)
+		wg.Done()
+		return
+	}
+
+	// Make a bunch of concurrent accumulating and non-accumulating contenders
+	for i := 0; i < contentionScale; i++ {
+		wg.Add(2)
+		go catchAndRelease(contentionScale)
+		go catchAndRelease(1)
+	}
+
+	// Close the closeCh when all contenders have finished
+	go func() {
+		wg.Wait()
+		close(closeCh)
+	}()
+
+	// Slowly release the full resources
+	for i := 0; i < contentionScale; i++ {
+		u.Release(1)
+	}
+
+	select {
+	case <-time.After(longTime):
+		s.Fail(`Accumulative acquirers should win in reasonable time`)
+		close(closeCh)
+		u.Release(contentionScale * contentionScale)
+	case <-closeCh:
+	}
+}
+
+func (s *UnboundedSemaphoreSuite) TestOneShotAccumulativeAcquires() {
+	var u UnboundedSemaphore
+	var closeCh chan struct{}
+
+	acquireAndClose := func(i int, c chan struct{}) {
+		u.Acquire(i)
+		close(c)
+	}
+
+	u.Release(1000)
+	u.Acquire(700) // Both of these Acquires are fully satisfied in one-pass
+	u.Acquire(300)
+
+	closeCh = make(chan struct{}, 0)
+	go acquireAndClose(1000, closeCh)
+	select {
+	case <-time.After(shortTime):
+	case <-closeCh:
+		s.Fail(`Acquire should have failed`)
+	}
+
+	closeCh = make(chan struct{}, 0)
+	go acquireAndClose(1, closeCh)
+	select {
+	case <-time.After(shortTime):
+	case <-closeCh:
+		s.Fail(`Acquire should have failed`)
+	}
+
+	closeCh = make(chan struct{}, 0)
+	go acquireAndClose(1000, closeCh)
+	u.Release(2001)
+	select {
+	case <-time.After(shortTime):
+		s.Fail(`Acquire should have succeeded`)
+	case <-closeCh:
+	}
+
+	u.Release(100000)
+	<-closeCh
+}
+
+func (s *UnboundedSemaphoreSuite) TestConcurrentReleases() {
+	contentionScale := 1000
+	total := 0
+	var u UnboundedSemaphore
+	var wg sync.WaitGroup
+	closeCh := make(chan struct{}, 0)
+
+	acquireAndClose := func(i int, c chan struct{}) {
+		u.Acquire(i)
+		close(c)
+	}
+
+	// one-shot synchronized releaser
+	release := func(i int) {
+		wg.Wait()
+		u.Release(i)
+		return
+	}
+
+	wg.Add(1)
+
+	// Make a bunch of concurrent releasers
+	for i := 0; i < contentionScale; i++ {
+		total += i
+		go release(i)
+	}
+
+	// Try to acquire the full amount in one shot
+	go acquireAndClose(total, closeCh)
+
+	// Bang!
+	wg.Done()
+
+	select {
+	case <-time.After(longTime):
+		s.Fail(`Synchornized releasers should release an acquirer in reasonable time`)
+		close(closeCh)
+		u.Release(contentionScale * contentionScale)
+	case <-closeCh:
+	}
+	
+	closeCh = make(chan struct{}, 0)
+	go acquireAndClose(1, closeCh)
+	select {
+	case <-time.After(shortTime):
+	case <-closeCh:
+		s.Fail(`Acquire should have failed`)
+	}
+}
