f98f0e24e409093f065b31711104d50acb43dc7d
kiranrg <kiranrg@uber.com>
2017-10-12 15:48:50 -0700

    100ms redelivery-ticker

--- services/outputhost/messagecache.go
+++ services/outputhost/messagecache.go
@@ -164,7 +164,7 @@ type AckID string
 const defaultLockTimeoutInSeconds int32 = 42
 const defaultMaxDeliveryCount int32 = 2
 const blockCheckingTimeout time.Duration = time.Minute
-const redeliveryInterval = time.Second / 2
+const redeliveryInterval = 100 * time.Millisecond
 
 // SmartRetryDisableString can be added to a destination or CG owner email to request smart retry to be disabled
 // Note that Google allows something like this: gbailey+smartRetryDisable@uber.com
