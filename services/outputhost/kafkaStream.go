5df6bf3d89f0ccb9bde866d207e171fd5d511d9e
Xu Ning <nx@uber.com>
2017-11-18 18:42:17 -0800

    Sequence number should be outside of message converter

--- services/outputhost/kafkaStream.go
+++ services/outputhost/kafkaStream.go
@@ -94,12 +94,18 @@ func (k *kafkaStream) Read() (*store.ReadMessageContent, error) {
 		return nil, errKafkaClosed
 	}
 	k.creditSemaphore.Acquire(1) // TODO: Size-based credits
-	return k.kafkaConverter(m), nil
+	c := k.kafkaConverter(m)
+	c.Message.Message.SequenceNumber = common.Int64Ptr(k.getNextSequenceNumber())
+	return c, nil
 }
 
 // ResponseHeaders returns the response headers sent from the server. This will block until server headers have been received.
 func (k *kafkaStream) ResponseHeaders() (map[string]string, error) {
 	return nil, errors.New(`unimplemented`)
+}
+
+func (k *kafkaStream) getNextSequenceNumber() int64 {
+	return atomic.AddInt64(&k.seqNo, 1)
 }
 
 /*
@@ -114,13 +120,13 @@ func OpenKafkaStream(c <-chan *s.ConsumerMessage, kafkaMessageConverter KafkaMes
 		kafkaConverter: kafkaMessageConverter,
 	}
 	if k.kafkaConverter == nil {
-		k.kafkaConverter = GetDefaultKafkaMessageConverter(&k.seqNo, k.logger)
+		k.kafkaConverter = GetDefaultKafkaMessageConverter(k.logger)
 	}
 	return k
 }
 
 // GetDefaultKafkaMessageConverter returns the default kafka message converter
-func GetDefaultKafkaMessageConverter(seqNo *int64, logger bark.Logger) KafkaMessageConverter {
+func GetDefaultKafkaMessageConverter(logger bark.Logger) KafkaMessageConverter {
 	return func(m *s.ConsumerMessage) (c *store.ReadMessageContent) {
 		c = &store.ReadMessageContent{
 			Type: store.ReadMessageContentTypePtr(store.ReadMessageContentType_MESSAGE),
@@ -145,7 +151,16 @@ func GetDefaultKafkaMessageConverter(seqNo *int64, logger bark.Logger) KafkaMess
 		}
 
 		c.Message.Message = &store.AppendMessage{
-			SequenceNumber: common.Int64Ptr(atomic.AddInt64(seqNo, 1)),
+			Payload: &cherami.PutMessage{
+				Data: m.Value,
+				UserContext: map[string]string{
+					`key`:       string(m.Key),
+					`topic`:     m.Topic,
+					`partition`: strconv.Itoa(int(m.Partition)),
+					`offset`:    strconv.Itoa(int(m.Offset)),
+				},
+				// TODO: Checksum?
+			},
 		}
 
 		if !m.Timestamp.IsZero() {
@@ -153,16 +168,6 @@ func GetDefaultKafkaMessageConverter(seqNo *int64, logger bark.Logger) KafkaMess
 			c.Message.Message.EnqueueTimeUtc = common.Int64Ptr(m.Timestamp.UnixNano())
 		}
 
-		c.Message.Message.Payload = &cherami.PutMessage{
-			Data: m.Value,
-			UserContext: map[string]string{
-				`key`:       string(m.Key),
-				`topic`:     m.Topic,
-				`partition`: strconv.Itoa(int(m.Partition)),
-				`offset`:    strconv.Itoa(int(m.Offset)),
-			},
-			// TODO: Checksum?
-		}
 		return c
 	}
 }
